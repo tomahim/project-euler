098acbf8c0f89983307891ed1b6a787fee2078cd
tomabolt <thimblot@gmail.com>
2019-01-27 19:05:04 +0100

    Add documentation

--- python_solutions/problem1.py
+++ python_solutions/problem1.py
@@ -15,7 +15,6 @@ def sum_multiples_3_and_5_optimized(max):
         p = math.floor((max - 1) / divisible_by)
         return math.floor(divisible_by * (p * (p + 1)) / 2)
 
-
     return (
         sum_divisible_by(max, 3) +
         sum_divisible_by(max, 5) -
@@ -24,6 +23,12 @@ def sum_multiples_3_and_5_optimized(max):
 
 
 if __name__ == '__main__':
+    """
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+    """
+
     assert sum_multiples_3_and_5_naive(10) == 23
 
     expected_result = 233168
